92d845991048947f1bcd64e5d2076e00bc0f7af0
yachaverrc <60229713+yachaverrc@users.noreply.github.com>
2023-05-19 16:01:14 -0500

    Update config.py

--- Master/ControllerASG/config.py
+++ Master/ControllerASG/config.py
@@ -1,4 +1,6 @@
 aws_access_key_id = 'ASIAXG6HUWADB4SJ6J6Q'
 aws_secret_access_key = 'LnTUel7ZdIAbUB19cPq3GctDtsKUb7CSjdQYLGde'
+aws_session_token = ''
 aws_region = 'us-east-1'
 id_AMI = 'ami-0f0f232eb233dfb63' 
+
