0c6f86325ed5c3869830d1027edd54b33e968658
root <root@DESKTOP-V1AO4B2.localdomain>
2023-05-19 10:37:06 -0500

    test credenciales

--- Master/ControllerASG/config.py
+++ Master/ControllerASG/config.py
@@ -1,3 +1,3 @@
-aws_access_key_id = ''
-aws_secret_access_key = ''
+aws_access_key_id = 'ASIAXG6HUWADB4SJ6J6Q'
+aws_secret_access_key = 'LnTUel7ZdIAbUB19cPq3GctDtsKUb7CSjdQYLGde'
 aws_region = 'us-east-1'
